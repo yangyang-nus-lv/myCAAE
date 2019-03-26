833e7e611914a307947985987acadce96ebcbe84
Yang Yang <yang_yang@u.nus.edu>
2019-03-26 22:11:33 +0800

    loss track

--- model.py
+++ model.py
@@ -200,7 +200,7 @@ class CAAE(object):
         self.dz_optimizer = Adam(self.Dz.parameters())
         self.di_optimizer = Adam(self.Dimg.parameters())
 
-        self.device = 'cuda:1'
+        self.device = None
 
     def __call__(self, *args, **kwargs):
         self.test_single(*args, **kwargs)
